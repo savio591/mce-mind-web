1f38530a353349c17d02227df9c3d75d316be2c5
savio591 <savio591@hotmail.com>
2021-07-16 17:10:24 -0300

    Component: Button #8

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,35 @@
+import { ReactNode } from 'react';
+
+import styles from './Button.module.scss';
+
+export interface ButtonProps {
+  children: ReactNode;
+  size?: 'default' | 'small';
+  style?: 'default' | 'outline';
+  type?: 'default' | 'icon';
+  isLoading?: boolean;
+  onClick: () => void;
+}
+
+export function Button({
+  children,
+  size,
+  style,
+  type,
+  isLoading,
+  onClick,
+}: ButtonProps): JSX.Element {
+  return (
+    <button
+      type="button"
+      data-size={size ?? 'default'}
+      data-style={style ?? 'default'}
+      data-type={type ?? 'default'}
+      className={styles.button}
+      onClick={onClick}
+      disabled={isLoading ?? false}
+    >
+      {children}
+    </button>
+  );
+}
