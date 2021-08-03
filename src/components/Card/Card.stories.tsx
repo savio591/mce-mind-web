68892fdbaec2573e5fb3e8e6b4af6c284d1b993e
savio591 <savio591@hotmail.com>
2021-08-03 09:13:45 -0300

    Components: Card #8 #3

---
+++ src/components/Card/Card.stories.tsx
@@ -0,0 +1,38 @@
+import { Meta, Story } from '@storybook/react';
+
+import { Card, CardProps } from '.';
+
+export default {
+  title: 'Components/Card',
+  component: Card,
+  argTypes: {
+    date: { control: 'date' },
+  },
+} as Meta;
+
+const Template: Story<CardProps> = args => <Card {...args} />;
+
+export const Default = Template.bind({});
+export const WithDate = Template.bind({});
+export const Small = Template.bind({});
+
+Default.args = {
+  // date: new Date(),
+  name: 'Savio Castelo',
+  phone: '+55 96 99112-3620',
+  image: 'https://github.com/savio591.png',
+};
+
+WithDate.args = {
+  date: new Date(),
+  name: 'Savio Castelo',
+  phone: '+55 96 99112-3620',
+  image: 'https://github.com/savio591.png',
+};
+
+Small.args = {
+  ...Default.args,
+  phone: undefined,
+  date: undefined,
+  size: 'small',
+};
