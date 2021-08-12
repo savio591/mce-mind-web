c2b6054f95704f191cf64ea59d11910ac24fdc5f
savio591 <savio591@hotmail.com>
2021-08-12 17:30:56 -0300

    API: Providers and Appointments #6

---
+++ src/pages/api/signin.ts
@@ -0,0 +1,52 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { query as q } from 'faunadb';
+
+import { fql } from '../../services/fauna';
+import { validateAuth } from '../../utils/validateInputs';
+
+interface FQLLoginResponse {
+  secret: string;
+  instance: {
+    id: string;
+  };
+}
+
+export default async function signIn(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> {
+  const isPostRequest = req.method === 'POST';
+  const isJson = req.headers['content-type'] === 'application/json';
+
+  if (isPostRequest && isJson) {
+    try {
+      const { email, password } = req.body;
+      const isRequestValid = validateAuth(req.body);
+
+      if (!isRequestValid) {
+        throw new Error();
+      }
+
+      const { secret, instance: userRef } = await fql.query<FQLLoginResponse>(
+        q.Login(q.Match(q.Index('user_by_email'), q.Casefold(email)), {
+          password,
+        })
+      );
+
+      res.status(200).json({ email, secret, profileId: userRef.id });
+
+      // it will block process cascading.
+      return;
+    } catch {
+      res.status(404).json({ error: 'invalid email/password' });
+      return;
+    }
+  }
+
+  if (!isJson) {
+    res.status(401).json({ error: 'this content type is not allowed' });
+    return;
+  }
+  res.setHeader('Allow', 'POST');
+  res.status(405).end('Method not allowed');
+}
