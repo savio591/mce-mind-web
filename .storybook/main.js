60731abd58bb5ecf3c10056e32ab3219e7631fa3
savio591 <savio591@hotmail.com>
2021-08-21 20:22:41 -0300

    Storybook: Core upgrade(fixed ES modules bug) #3

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,32 @@
-const path = require('path');
-
 module.exports = {
-  core: {
-    builder: 'webpack5',
-  },
-
-  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-a11y',
   ],
-  presets: [path.resolve(__dirname, './next-preset.js')],
+  core: {
+    builder: 'webpack5',
+  },
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  webpackFinal: async (config /* , { configType } */) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.module.rules.push({
+      test: /\.s[ac]ss$/i,
+      use: [
+        // Creates `style` nodes from JS strings
+        'style-loader',
+        // Translates CSS into CommonJS
+        'css-loader',
+        // Compiles Sass to CSS
+        'sass-loader',
+      ],
+    });
+
+    // Return the altered config
+    return config;
+  },
 };
